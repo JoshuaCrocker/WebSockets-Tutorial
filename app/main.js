f062ffa7f7af5b633404a0469a3c15f59aa0fbb6
Joshua Crocker <joshua@auratemple.net>
2015-09-26 20:45:24 +0100

    Updating like counts

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 var userId = localStorage.getItem('userId') ||  randomId();
 localStorage.setItem('userId', userId);
 console.info("Hi i'm user #"+userId);
+var messageCache;
 
 function randomId() {
   return Math.floor(Math.random() * 1e11);
@@ -10,19 +11,27 @@ var socket = io.connect('http://localhost:3000',  { forceNew: true });
 
 socket.on('messages', function(data) {
   console.info(data);
+  messageCache = data;
+  render();
+});
 
-  var html = data.map(function(data) {
+function render() {
+  var data = messageCache;
+
+  var html = data.map(function(data, index) {
     return (`
-        <div class="name">
-          ${data.userName}
-        </div>
-        <a href=${data.content.link} class="message" target="blank">${data.content.text}</a>
-        <input type="submit" class="likes-count" value="${data.likedBy.length} Enjoys">
+        <form class="message" onsubmit="return likeMessage(messageCache[${index}])">
+          <div class="name">
+            ${data.userName}
+          </div>
+          <a href=${data.content.link} class="message" target="blank">${data.content.text}</a>
+          <input type="submit" class="likes-count" value="${data.likedBy.length} Enjoys">
+        </form>
       `);
   }).join(' ');
 
   document.getElementById('messages').innerHTML = html;
-});
+}
 
 function addMessage(e) {
   var username = document.getElementById('username'),
@@ -44,5 +53,19 @@ function addMessage(e) {
   linkAddress.value = '';
   message.value = '';
 
+  return false;
+}
+
+function likeMessage(message) {
+  var index = message.likedBy.indexOf(userId);
+
+  if (index < 0) {
+    message.likedBy.push(userId);
+  } else {
+    message.likedBy.splice(index,1);
+  }
+
+  socket.emit('update-message', message);
+  render();
   return false;
 }
