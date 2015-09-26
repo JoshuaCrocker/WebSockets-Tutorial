24c0bbcf9089a5189baa31160c58dffa5478ef7b
Joshua Crocker <joshua@auratemple.net>
2015-09-26 20:16:05 +0100

    Displayed messages

--- server/main.js
+++ server/main.js
@@ -3,6 +3,40 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+var messages = [{
+  userId:1,
+  messageId:10,
+  userName:'Asha Greyjoy',
+  content: {
+    text: 'The stone tree of Stonetrees.',
+    link: 'http://awoiaf.westeros.org/index.php/House_Stonetree'
+  },
+  likedBy:[1],
+  ts: Date.now() - 10000
+},
+{
+  userId:2,
+  messageId:11,
+  userName:'Arya Stark',
+  content: {
+    text: 'We\'ll come see this inn.',
+    link: 'http://gameofthrones.wikia.com/wiki/Inn_at_the_Crossroads'
+  },
+  likedBy:[2,3],
+  ts: Date.now() - 10000
+},
+{
+  userId:3,
+  messageId:14,
+  userName:'Cersei Lannister',
+  content: {
+    text: 'Her scheming forced this on me.',
+    link: 'http://gameofthones.wikia.com/wiki/Margaery_Tyrell'
+  },
+  likedBy:[],
+  ts: Date.now() - 10000
+}];
+
 // app.get('/', function(req, res) {
 //   res.send('Hello World.');
 //   console.log('Something connected to express.');
@@ -12,7 +46,7 @@ app.use(express.static('app'));
 
 io.on('connection', function(socket) {
   console.log('Something connected to Socket.io');
-  socket.emit('messages', ['Hello', 'Hi there', 'How are you?']);
+  socket.emit('messages', messages);
 });
 
 server.listen(3000);
